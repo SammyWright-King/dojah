d47ec2e183b8d8b4203065d90dc5697b0d6deeec
Olumuyiwa <olumuyiwa@elta.solutions>
2020-12-07 13:36:31 +0100

    modifications to dojah package on setting and getting parameters

--- src/Dojah.php
+++ src/Dojah.php
@@ -4,24 +4,65 @@ namespace muyiwa\dojah;
 
 class Dojah{
     public $baseUrl;
-    private $dojah_key;
+    private $dojah_secret_key;
+    private $appId;
 
-    public function __construct(){
-        $this->baseUrl = env('DOJAH_BASE_URL');
-        $this->dojah_secret_key = env('DOJAH_SECRET_KEY');
+
+    //set base url
+    public function setBaseUrl($base_url){
+        $this->baseUrl = $base_url;
+    }
+
+    //public get base url
+    public function getBaseUrl(){
+        return $this->baseUrl;
+    }
+
+    //set dojah app id
+    public function setAppId($app_id){
+        $this->appId = $app_id;
+    }
+
+    //get dojah app id
+    public function getAppId(){
+        return $this->appId;
+    }
+
+    //set dojah secret key
+    public function setSecretkey($secret_key){
+        $this->dojah_secret_key = $secret_key;
+    }
+
+    //get dojah secret key
+    public function getSecretkey(){
+        return $this->dojah_secret_key;
+    }
+
+    //set dojah header parameters
+    public function setHeader($secretKey, $app_id){
+        $this->dojah_secret_key = $secretKey;
+        $this->appId = $app_id;
+    }
+
+    //get dojah header parameters
+    public function getHeader(){
+        return [
+             $this->dojah_secret_key,
+             $this->appId
+        ];
     }
 
     //fetch balance
     public function fetchBalance(){
 
-        $url = env('DOJAH_BASE_URL'). "/api/v1/balance";
+        $url = $this->baseUrl. "/api/v1/balance";
         $cURLConnection = curl_init();
 
         curl_setopt($cURLConnection, CURLOPT_URL, $url);
         curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, array(
-            "Authorization: ". env('DOJAH_SECRET_KEY'),
-            "AppId: " .env('DOJAH_APP_ID'),
+            "Authorization: ". $this->dojah_secret_key ,
+            "AppId: ". $this->appId,
         ));
 
         $balance = curl_exec($cURLConnection);
